20c19e11bc9a91f18763404a35ac5bac24ea8985
william.o'dell <william.o'dell@uspsector.com>
2020-02-14 14:36:29 -0800

    Implement buffer list

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,194 +27,193 @@ Partial Class Form1
     Me.Label1 = New System.Windows.Forms.Label()
     Me.splMain = New System.Windows.Forms.SplitContainer()
     Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-    Me.Panel1 = New System.Windows.Forms.Panel()
-    Me.btnClearClipboard = New System.Windows.Forms.Button()
-    Me.btnClearBuffer = New System.Windows.Forms.Button()
-    Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-    Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
-    Me.ssMain = New System.Windows.Forms.StatusStrip()
-    Me.tsslStatus = New System.Windows.Forms.ToolStripStatusLabel()
-    Me.tsslCount = New System.Windows.Forms.ToolStripStatusLabel()
-    Me.tsslCmd = New System.Windows.Forms.ToolStripStatusLabel()
-    Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
-    Me.Panel2 = New System.Windows.Forms.Panel()
-    Me.lbxClipboardBuffer = New System.Windows.Forms.ListBox()
-    CType(Me.splMain, System.ComponentModel.ISupportInitialize).BeginInit()
-    Me.splMain.Panel1.SuspendLayout()
-    Me.splMain.Panel2.SuspendLayout()
-    Me.splMain.SuspendLayout()
-    Me.GroupBox1.SuspendLayout()
-    Me.Panel1.SuspendLayout()
-    Me.ssMain.SuspendLayout()
-    Me.Panel2.SuspendLayout()
-    Me.SuspendLayout()
-    '
-    'lblCBContents
-    '
-    Me.lblCBContents.AutoSize = True
-    Me.lblCBContents.Location = New System.Drawing.Point(106, 8)
-    Me.lblCBContents.Name = "lblCBContents"
-    Me.lblCBContents.Size = New System.Drawing.Size(33, 13)
-    Me.lblCBContents.TabIndex = 0
-    Me.lblCBContents.Text = "None"
-    '
-    'Label1
-    '
-    Me.Label1.AutoSize = True
-    Me.Label1.Location = New System.Drawing.Point(12, 8)
-    Me.Label1.Name = "Label1"
-    Me.Label1.Size = New System.Drawing.Size(88, 13)
-    Me.Label1.TabIndex = 0
-    Me.Label1.Text = "Current Clipboard"
-    '
-    'splMain
-    '
-    Me.splMain.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.splMain.Location = New System.Drawing.Point(0, 0)
-    Me.splMain.Name = "splMain"
-    Me.splMain.Orientation = System.Windows.Forms.Orientation.Horizontal
-    '
-    'splMain.Panel1
-    '
-    Me.splMain.Panel1.Controls.Add(Me.lblCBContents)
-    Me.splMain.Panel1.Controls.Add(Me.Label1)
-    '
-    'splMain.Panel2
-    '
-    Me.splMain.Panel2.Controls.Add(Me.GroupBox1)
-    Me.splMain.Size = New System.Drawing.Size(386, 354)
-    Me.splMain.SplitterDistance = 31
-    Me.splMain.SplitterWidth = 10
-    Me.splMain.TabIndex = 3
-    '
-    'GroupBox1
-    '
-    Me.GroupBox1.Controls.Add(Me.Panel2)
-    Me.GroupBox1.Controls.Add(Me.Panel1)
-    Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
-    Me.GroupBox1.Name = "GroupBox1"
-    Me.GroupBox1.Size = New System.Drawing.Size(386, 313)
-    Me.GroupBox1.TabIndex = 4
-    Me.GroupBox1.TabStop = False
-    Me.GroupBox1.Text = "Clipboard History"
-    '
-    'Panel1
-    '
-    Me.Panel1.Controls.Add(Me.btnClearClipboard)
-    Me.Panel1.Controls.Add(Me.btnClearBuffer)
-    Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
-    Me.Panel1.Location = New System.Drawing.Point(3, 16)
-    Me.Panel1.Name = "Panel1"
-    Me.Panel1.Size = New System.Drawing.Size(380, 29)
-    Me.Panel1.TabIndex = 6
-    '
-    'btnClearClipboard
-    '
-    Me.btnClearClipboard.Dock = System.Windows.Forms.DockStyle.Left
-    Me.btnClearClipboard.Location = New System.Drawing.Point(75, 0)
-    Me.btnClearClipboard.Name = "btnClearClipboard"
-    Me.btnClearClipboard.Size = New System.Drawing.Size(87, 29)
-    Me.btnClearClipboard.TabIndex = 7
-    Me.btnClearClipboard.Text = "Clear Clipboard"
-    Me.btnClearClipboard.UseVisualStyleBackColor = True
-    '
-    'btnClearBuffer
-    '
-    Me.btnClearBuffer.Dock = System.Windows.Forms.DockStyle.Left
-    Me.btnClearBuffer.Location = New System.Drawing.Point(0, 0)
-    Me.btnClearBuffer.Name = "btnClearBuffer"
-    Me.btnClearBuffer.Size = New System.Drawing.Size(75, 29)
-    Me.btnClearBuffer.TabIndex = 6
-    Me.btnClearBuffer.Text = "Clear Buffer"
-    Me.btnClearBuffer.UseVisualStyleBackColor = True
-    '
-    'Timer1
-    '
-    Me.Timer1.Enabled = True
-    Me.Timer1.Interval = 10
-    '
-    'NotifyIcon1
-    '
-    Me.NotifyIcon1.Text = "NotifyIcon1"
-    Me.NotifyIcon1.Visible = True
-    '
-    'ssMain
-    '
-    Me.ssMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsslStatus, Me.tsslCount, Me.tsslCmd})
-    Me.ssMain.Location = New System.Drawing.Point(0, 332)
-    Me.ssMain.Name = "ssMain"
-    Me.ssMain.Size = New System.Drawing.Size(386, 22)
-    Me.ssMain.TabIndex = 4
-    Me.ssMain.Text = "StatusStrip1"
-    '
-    'tsslStatus
-    '
-    Me.tsslStatus.Name = "tsslStatus"
-    Me.tsslStatus.Size = New System.Drawing.Size(39, 17)
-    Me.tsslStatus.Text = "Ready"
-    '
-    'tsslCount
-    '
-    Me.tsslCount.Name = "tsslCount"
-    Me.tsslCount.Size = New System.Drawing.Size(21, 17)
-    Me.tsslCount.Text = "##"
-    '
-    'tsslCmd
-    '
-    Me.tsslCmd.Name = "tsslCmd"
-    Me.tsslCmd.Size = New System.Drawing.Size(33, 17)
-    Me.tsslCmd.Text = "Cmd"
-    '
-    'ToolTip1
-    '
-    '
-    'Panel2
-    '
-    Me.Panel2.Controls.Add(Me.lbxClipboardBuffer)
-    Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.Panel2.Location = New System.Drawing.Point(3, 45)
-    Me.Panel2.Name = "Panel2"
-    Me.Panel2.Size = New System.Drawing.Size(380, 265)
-    Me.Panel2.TabIndex = 7
-    '
-    'lbxClipboardBuffer
-    '
-    Me.lbxClipboardBuffer.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.lbxClipboardBuffer.FormattingEnabled = True
-    Me.lbxClipboardBuffer.HorizontalScrollbar = True
-    Me.lbxClipboardBuffer.Items.AddRange(New Object() {"Test"})
-    Me.lbxClipboardBuffer.Location = New System.Drawing.Point(0, 0)
-    Me.lbxClipboardBuffer.Name = "lbxClipboardBuffer"
-    Me.lbxClipboardBuffer.ScrollAlwaysVisible = True
-    Me.lbxClipboardBuffer.Size = New System.Drawing.Size(380, 265)
-    Me.lbxClipboardBuffer.TabIndex = 5
-    '
-    'Form1
-    '
-    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-    Me.ClientSize = New System.Drawing.Size(386, 354)
-    Me.Controls.Add(Me.ssMain)
-    Me.Controls.Add(Me.splMain)
-    Me.Name = "Form1"
-    Me.Text = "Form1"
-    Me.splMain.Panel1.ResumeLayout(False)
-    Me.splMain.Panel1.PerformLayout()
-    Me.splMain.Panel2.ResumeLayout(False)
-    CType(Me.splMain, System.ComponentModel.ISupportInitialize).EndInit()
-    Me.splMain.ResumeLayout(False)
-    Me.GroupBox1.ResumeLayout(False)
-    Me.Panel1.ResumeLayout(False)
-    Me.ssMain.ResumeLayout(False)
-    Me.ssMain.PerformLayout()
-    Me.Panel2.ResumeLayout(False)
-    Me.ResumeLayout(False)
-    Me.PerformLayout()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lbxClipboardBuffer = New System.Windows.Forms.ListBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnClearClipboard = New System.Windows.Forms.Button()
+        Me.btnClearBuffer = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ssMain = New System.Windows.Forms.StatusStrip()
+        Me.tsslStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tsslCount = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tsslCmd = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        CType(Me.splMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.splMain.Panel1.SuspendLayout()
+        Me.splMain.Panel2.SuspendLayout()
+        Me.splMain.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.ssMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblCBContents
+        '
+        Me.lblCBContents.AutoSize = True
+        Me.lblCBContents.Location = New System.Drawing.Point(106, 8)
+        Me.lblCBContents.Name = "lblCBContents"
+        Me.lblCBContents.Size = New System.Drawing.Size(33, 13)
+        Me.lblCBContents.TabIndex = 0
+        Me.lblCBContents.Text = "None"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Current Clipboard"
+        '
+        'splMain
+        '
+        Me.splMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.splMain.Location = New System.Drawing.Point(0, 0)
+        Me.splMain.Name = "splMain"
+        Me.splMain.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'splMain.Panel1
+        '
+        Me.splMain.Panel1.Controls.Add(Me.lblCBContents)
+        Me.splMain.Panel1.Controls.Add(Me.Label1)
+        '
+        'splMain.Panel2
+        '
+        Me.splMain.Panel2.Controls.Add(Me.GroupBox1)
+        Me.splMain.Size = New System.Drawing.Size(386, 354)
+        Me.splMain.SplitterDistance = 31
+        Me.splMain.SplitterWidth = 10
+        Me.splMain.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Panel2)
+        Me.GroupBox1.Controls.Add(Me.Panel1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(386, 313)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Clipboard History"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.lbxClipboardBuffer)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(3, 45)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(380, 265)
+        Me.Panel2.TabIndex = 7
+        '
+        'lbxClipboardBuffer
+        '
+        Me.lbxClipboardBuffer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lbxClipboardBuffer.FormattingEnabled = True
+        Me.lbxClipboardBuffer.HorizontalScrollbar = True
+        Me.lbxClipboardBuffer.Location = New System.Drawing.Point(0, 0)
+        Me.lbxClipboardBuffer.Name = "lbxClipboardBuffer"
+        Me.lbxClipboardBuffer.ScrollAlwaysVisible = True
+        Me.lbxClipboardBuffer.Size = New System.Drawing.Size(380, 265)
+        Me.lbxClipboardBuffer.TabIndex = 5
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnClearClipboard)
+        Me.Panel1.Controls.Add(Me.btnClearBuffer)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(3, 16)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(380, 29)
+        Me.Panel1.TabIndex = 6
+        '
+        'btnClearClipboard
+        '
+        Me.btnClearClipboard.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnClearClipboard.Location = New System.Drawing.Point(75, 0)
+        Me.btnClearClipboard.Name = "btnClearClipboard"
+        Me.btnClearClipboard.Size = New System.Drawing.Size(87, 29)
+        Me.btnClearClipboard.TabIndex = 7
+        Me.btnClearClipboard.Text = "Clear Clipboard"
+        Me.btnClearClipboard.UseVisualStyleBackColor = True
+        '
+        'btnClearBuffer
+        '
+        Me.btnClearBuffer.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnClearBuffer.Location = New System.Drawing.Point(0, 0)
+        Me.btnClearBuffer.Name = "btnClearBuffer"
+        Me.btnClearBuffer.Size = New System.Drawing.Size(75, 29)
+        Me.btnClearBuffer.TabIndex = 6
+        Me.btnClearBuffer.Text = "Clear Buffer"
+        Me.btnClearBuffer.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ssMain
+        '
+        Me.ssMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsslStatus, Me.tsslCount, Me.tsslCmd})
+        Me.ssMain.Location = New System.Drawing.Point(0, 332)
+        Me.ssMain.Name = "ssMain"
+        Me.ssMain.Size = New System.Drawing.Size(386, 22)
+        Me.ssMain.TabIndex = 4
+        Me.ssMain.Text = "StatusStrip1"
+        '
+        'tsslStatus
+        '
+        Me.tsslStatus.Name = "tsslStatus"
+        Me.tsslStatus.Size = New System.Drawing.Size(39, 17)
+        Me.tsslStatus.Text = "Ready"
+        '
+        'tsslCount
+        '
+        Me.tsslCount.Name = "tsslCount"
+        Me.tsslCount.Size = New System.Drawing.Size(21, 17)
+        Me.tsslCount.Text = "##"
+        '
+        'tsslCmd
+        '
+        Me.tsslCmd.Name = "tsslCmd"
+        Me.tsslCmd.Size = New System.Drawing.Size(33, 17)
+        Me.tsslCmd.Text = "Cmd"
+        '
+        'ToolTip1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(386, 354)
+        Me.Controls.Add(Me.ssMain)
+        Me.Controls.Add(Me.splMain)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.splMain.Panel1.ResumeLayout(False)
+        Me.splMain.Panel1.PerformLayout()
+        Me.splMain.Panel2.ResumeLayout(False)
+        CType(Me.splMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.splMain.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.ssMain.ResumeLayout(False)
+        Me.ssMain.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
-  End Sub
+    End Sub
 
-  Friend WithEvents lblCBContents As Label
+    Friend WithEvents lblCBContents As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents splMain As SplitContainer
     Friend WithEvents GroupBox1 As GroupBox
--- Form1.vb
+++ Form1.vb
@@ -2,16 +2,27 @@
   Dim cbData As IDataObject = Clipboard.GetDataObject()
   Dim cbText As String = ""
   Dim iCount As Integer = 0
+  Dim buffer As IList(Of Object)
 
+  '----------------------------------------------------------------------------
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    buffer = New List(Of Object)
+  End Sub
+
+  '----------------------------------------------------------------------------
   Private Sub getClipboardContent()
-
+    Dim wrappedString As String
     If My.Computer.Clipboard.ContainsAudio() Then
       tsslCmd.Text = "Audio"
     ElseIf My.Computer.Clipboard.ContainsData(DataFormats.Html) Then
       If cbText <> My.Computer.Clipboard.GetText() Then
         cbText = My.Computer.Clipboard.GetText()
         lblCBContents.Text = cbText
-        lbxClipboardBuffer.Items.Insert(0, "<" + cbText + ">")
+        wrappedString = "<" + cbText + ">"
+        Dim tmpList As IList(Of Object) = New List(Of Object)({cbText, wrappedString})
+
+        buffer.Insert(0, tmpList)
+        lbxClipboardBuffer.Items.Insert(0, wrappedString)
         tsslCmd.Text = "HTML"
       Else
         lblCBContents.Text = "HTML: No change: " + cbText
@@ -23,7 +34,11 @@
       If cbText <> My.Computer.Clipboard.GetText() Then
         cbText = My.Computer.Clipboard.GetText()
         lblCBContents.Text = cbText
-        lbxClipboardBuffer.Items.Insert(0, "<" + cbText + ">")
+        wrappedString = "<" + cbText + ">"
+        Dim tmpList As IList(Of Object) = New List(Of Object)({cbText, wrappedString})
+
+        buffer.Insert(0, tmpList)
+        lbxClipboardBuffer.Items.Insert(0, wrappedString)
         tsslCmd.Text = "Text"
       Else
         lblCBContents.Text = "No change:" + cbText
@@ -35,11 +50,12 @@
 
   End Sub
 
-
+  '----------------------------------------------------------------------------
   Private Sub btnGetCB_Click(sender As Object, e As EventArgs)
     getClipboardContent()
   End Sub
 
+  '----------------------------------------------------------------------------
   Private Sub hndlClipboardChanged(sender As Object, e As EventArgs) Handles Timer1.Tick
     getClipboardContent()
     iCount += 1
@@ -47,15 +63,26 @@
   End Sub
 
 
-
+  '----------------------------------------------------------------------------
   Private Sub btnClearBuffer_Click(sender As Object, e As EventArgs) Handles btnClearBuffer.Click
     lbxClipboardBuffer.Items.Clear()
     lblCBContents.Text = "Clear Buffer"
     cbText = ""
   End Sub
 
+  '----------------------------------------------------------------------------
   Private Sub lbxClipboardBuffer_DoubleClick(sender As Object, e As EventArgs) Handles lbxClipboardBuffer.DoubleClick
-    cbText = lbxClipboardBuffer.Text
+    Dim idx As Integer = lbxClipboardBuffer.SelectedIndex
+    Dim tmpObj = buffer.Item(idx)
+    'cbText = buffer.Item(idx).ToString
+    Dim n As Integer = 0
+    For Each i As Object In tmpObj
+      If n = 0 Then
+        cbText = i.ToString
+      End If
+
+      n += 1
+    Next
     If cbText <> "" Then
       My.Computer.Clipboard.SetText(cbText)
       tsslCmd.Text = cbText
@@ -63,6 +90,7 @@
 
   End Sub
 
+  '----------------------------------------------------------------------------
   Private Sub btnClearClipboard_Click(sender As Object, e As EventArgs) Handles btnClearClipboard.Click
     cbText = ""
     My.Computer.Clipboard.Clear()
@@ -70,11 +98,14 @@
     tsslCmd.Text = "Clear Clipboard"
   End Sub
 
+  '----------------------------------------------------------------------------
   Private Sub ToolTip1_Popup(sender As Object, e As PopupEventArgs) Handles ToolTip1.Popup
 
   End Sub
 
+  '----------------------------------------------------------------------------
   Private Sub lbxClipboardBuffer_MouseHover(sender As Object, e As EventArgs)
     ToolTip1.SetToolTip(lbxClipboardBuffer, cbText)
   End Sub
+
 End Class
