65f215bcc70fbc6324447ef59b33e56f1f80a045
odiewan <odie@odiesystems.com>
2020-05-14 05:50:29 -0700

    Fix undef cbx ref

---
+++ util_logging.vb
@@ -0,0 +1,31 @@
+﻿Module util_logging
+
+  '----------------------------------------------------------------------------
+  Private Sub AddMsg(msg As String)
+    Dim prfx As String
+    Dim totMsg As String
+
+    '---Get the method name
+    prfx = (New System.Diagnostics.StackTrace).GetFrame(1).GetMethod.Name
+
+
+    If msg IsNot "" Then
+
+      If msg = "s" Then
+        totMsg = prfx + "->Start"
+
+      ElseIf msg = "d" Then
+        totMsg = prfx + "->Done"
+      Else
+
+        totMsg = prfx + "->" + msg
+      End If
+
+    Else
+      totMsg = prfx
+    End If
+
+    Debug.WriteLine(totMsg)
+    'lbxConsole.Items.Insert(0, totMsg)
+  End Sub
+End Module
